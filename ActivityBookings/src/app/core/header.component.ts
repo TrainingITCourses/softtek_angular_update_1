8c1d22038a4a22ae069cff47c41c2f94f1d0860d
Alberto Basalo <albertobasalo@hotmail.com>
2024-04-10 10:30:10 +0200

    feat: sync 1

--- ActivityBookings/src/app/core/header.component.ts
+++ ActivityBookings/src/app/core/header.component.ts
@@ -1,17 +1,23 @@
+import { UpperCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+
+// class FakeAuthService {
+//   user = 'fake';
+// }
+// providers:[{provide: AuthService, useClass: FakeAuthService}]
 
 @Component({
   selector: 'lab-header',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [UpperCasePipe],
   template: `
     <header>
       <nav>
         <ul>
           <li>
-            🏠 <a href="">{{ title }}</a>
+            🏠 <a href="">{{ title | uppercase }}</a>
           </li>
           <li>
             👤 <a href="">{{ user }}</a>
@@ -25,5 +31,4 @@ export class HeaderComponent {
   #authService: AuthService = inject(AuthService);
   title: string = 'Activity Bookings';
   user: string = this.#authService.user;
-  // constructor(private authService: AuthService) {}
 }
